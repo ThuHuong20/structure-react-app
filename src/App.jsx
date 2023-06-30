fd1055641421ba55cb2f23e5e8f342e53610e483
ThuHuong20 <huong210720@gmail.com>
2023-06-30 17:09:59 +0700

    navigate, Link, lazy load, route-dom, alias

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import './App.scss'
+import { Routes, Route } from 'react-router-dom';
+import LazyLoad from './LazyLoad'
+import Navbars from './components/Navbar/Navbar';
+
 function App() {
   return (
     <div className="App">
-      hello
+      <div className='app_container'>
+        <Navbars />
+        <Routes>
+          <Route path="" element={LazyLoad(() => import("@pages/Homes/Home"))()} />
+          <Route path="/about" element={LazyLoad(() => import("@pages/Abouts/About"))()} >
+            <Route path='my-infor' element={LazyLoad(() => import("@pages/Abouts/MyInfors/MyInfor"))()}></Route>
+          </Route>
+        </Routes>
+      </div>
+
     </div>
   );
 }
